be59bf033cbf50b9eebb776a737cf17ca8a0c65e
Ripan Gharami <164193188+Ripan2005@users.noreply.github.com>
2025-08-20 18:34:39 +0530

    Create Search in fully rotated sorted 2D matrix.cpp

---
+++ Search in fully rotated sorted 2D matrix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    bool searchMatrix(vector<vector<int>> &mat, int x) {
+        // code here
+        int n = mat.size(), m = mat[0].size();
+        int l = 0, r = n * m - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            int mv = mat[mid / m][mid % m];
+            int lv = mat[l / m][l % m];
+            int rv = mat[r / m][r % m];
+            if (mv == x) return true;
+            if (lv <= mv) {
+                
+                if (lv <= x && x < mv) r = mid - 1;
+                else l = mid + 1;
+                
+            } else {
+                if (mv < x && x <= rv) l = mid + 1;
+                else r = mid - 1;
+                
+            }
+        }
+        return false;
+    }
+};
