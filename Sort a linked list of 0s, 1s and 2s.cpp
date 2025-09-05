313ce85fc2591ae53c91893b632b4c0b0d8d982a
Ripan Gharami <164193188+Ripan2005@users.noreply.github.com>
2025-09-05 21:51:52 +0530

    Create Sort a linked list of 0s, 1s and 2s.cpp

---
+++ Sort a linked list of 0s, 1s and 2s.cpp
@@ -0,0 +1,43 @@
+/* Node is defined as
+  class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    Node* segregate(Node* head) {
+        // code here
+         int zeros=0,ones=0,twos=0;
+        Node *temp=head;
+        while(temp){
+            if(temp->data==0)zeros++;
+            else if(temp->data==1)ones++;
+            else twos++;
+            temp=temp->next;
+        }
+        temp=head;
+        while(zeros){
+            temp->data=0;
+            zeros--;
+            temp=temp->next;
+        }
+        while(ones){
+            temp->data=1;
+            ones--;
+            temp=temp->next;
+        }
+        while(twos){
+            temp->data=2;
+            twos--;
+            temp=temp->next;
+        }
+        return head;
+    }
+};
